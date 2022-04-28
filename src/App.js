63b9d9208f3433d666a42eac7ace1234c7701495
raghulkrishnan5001 <raghul.doodleblue@gmail.com>
2022-04-28 08:51:32 +0530

    'day10'

---
+++ src/App.js
@@ -0,0 +1,11 @@
+
+import Polk from './Polk'
+function App() {
+    return (
+      <div>
+        <Polk/>
+      </div>
+    )
+  }
+
+export default App;
---
+++ src/Polk.js
@@ -0,0 +1,521 @@
+import React from 'react'
+import {GoMail} from "react-icons/go";
+import { BiPhoneCall } from "react-icons/bi";
+import { TiSocialGooglePlusCircular } from "react-icons/ti";
+import { ImPinterest2 } from "react-icons/im";
+import { TiSocialTwitterCircular } from "react-icons/ti";
+import {RiFacebookCircleLine } from "react-icons/ri";
+import { BiBasketball } from "react-icons/bi";
+import log from './picture/log.png';
+import bd1 from './picture/bd1.jpg';
+import { BiBed } from "react-icons/bi";
+import { BiBath } from "react-icons/bi";
+import { AiFillCodepenSquare } from "react-icons/ai";
+import { BiTransferAlt } from "react-icons/bi";
+import { BsHeartFill } from "react-icons/bs";
+import bd2 from'./picture/bd2.jpg';
+import bd3 from './picture/bd3.jpg';
+import bd5 from './picture/bd5.jpg';
+import {AiOutlinePlusCircle} from "react-icons/ai";
+import signature from './picture/signature.jpeg';
+import rock from './picture/rock.jpg';
+import quotes from './picture/quotes.png';
+import str2 from'./picture/str2.jpg';
+import str3 from'./picture/str3.jpg';
+import str4 from'./picture/str4.jpg';
+import str5 from'./picture/str5.jpg';
+import str6 from'./picture/str6.jpg';
+import bul1 from'./picture/bul1.jpg';
+import bul2 from'./picture/bul2.jpg';
+import bul3 from'./picture/bul3.jpg';
+import bul4 from'./picture/bul4.jpg';
+import bul5 from'./picture/bul5.jpg';
+import linee from './picture/linee.png'
+import streets from'./picture/streets.jpg';
+import {  BsDot } from "react-icons/bs";
+import './Polk.css';
+
+function Polk(){
+    return(
+        <div className='firstmain'>
+        <div className='main'>
+    <div className='qw'>
+        <GoMail size='14'/>sample@sample.com</div>
+        <div className='er'>
+    <BiPhoneCall size='15'/> 00 1234 5678
+    </div>
+    <div className='gt'>
+    <TiSocialGooglePlusCircular size='28'/>
+    </div>
+    <div className='sw'><ImPinterest2 size='18'/></div>
+    <div className='by'><TiSocialTwitterCircular size='25'/></div>
+    <div className='za'><RiFacebookCircleLine size='20'/></div>
+    <div className='bn'><BiBasketball size='20'/></div>
+    <br/>
+    </div>
+    <div className='main2'>
+    <div className='la'><h2>wp</h2></div>
+        <div className='ng'><h1>/</h1></div>
+        <div className='lp'><h3>Real Estate</h3></div>
+        <div className='he'>HOME</div>
+        <div className='ab'>ABOUT</div>
+        <div className='ag'>AGENTS</div>
+        <div className='pr'>PROPERTIES</div>
+        <div className='bl'>BLOG</div>
+        <div className='ct'>CONTACT</div>
+        </div>
+
+
+        <div className='wth'>
+        <div className='main3'>
+          <img src='https://media.istockphoto.com/photos/happy-family-under-fake-roof-in-living-room-picture-id1279995963?k=20&m=1279995963&s=170667a&w=0&h=d1sFvzaB48yj6RoOy5OjxQVp_bJX3KIDkLh2KDkOj_A=' width='800' height='400'></img>
+        </div>
+        <div className='txtt'>
+            if you can dream, we can create it
+            </div>
+            <div className='rtr'>
+              find your house without
+            </div>
+            <div className='apa'>
+                any difficulties</div>
+            <div className='ast'>
+                <h3>________</h3></div>
+              <div className='yht'>  <h5>| </h5>
+            </div>
+            <div className='wer'>|</div>
+            <div className='kmn'>Know More</div>
+            <div className='dfe'>________</div>
+            <div className='qsc'>|</div>
+            <div className='ldn'>|</div>
+            <div className='qry'> <h4>01</h4> </div>
+            <div className='tgj'> | 05 . . . . . </div>
+        </div>
+        <div className='log'>
+        <img src={log} width='1200' height='100' ></img></div>
+
+        <div className='main4'>
+        <div className='hty'><h2>wp</h2></div>
+        <div className='jhg'><h1>/</h1></div>
+        <div className='bea'><h3>Real Estate</h3></div>
+        </div>
+
+        <div className='pts'>
+        Properties    
+            </div>
+
+            <div className='hmew'>
+                HOME  FOR  SALE
+            </div>
+
+            <div className='bud1'>
+                <img src={bd1}/>
+            </div>
+            <div className='main5'>
+            <div className='adse'>
+               <h2> Scandinavian style apartment</h2>
+            </div>
+            <div className='intr'>
+                <h4>94-98 Ingraham St,Brooklyn,NY 11237,USA</h4>
+            </div>
+
+            <div className='nhju'>
+            <BiBed size='15'/>
+            </div>
+
+            <div className='rgtf'>
+            Beds:3
+            </div>
+
+            <div className='bth'>
+            <BiBath size='15'/>
+            </div>
+
+            <div className='cdfr'>
+                Baths:2
+            </div>
+
+            <div className='qazx'>
+            <AiFillCodepenSquare size='15'/>
+            </div>
+
+            <div className='tgvc'> 
+                Sq Ft:900
+            </div>
+            <div className='plmn'>
+                <hr></hr>
+            </div>
+             
+             <div className='xcvb'>
+                 FOR RENT
+             </div>
+
+             <div className='lkjh'>
+                <h3> $54.000 </h3>
+             </div>
+
+             <div className='rfvg'>
+             <button><BiTransferAlt size='17'/></button>
+             </div>
+
+             <div className='poiu'>
+             <button><BsHeartFill size='17'/></button>
+             </div>
+             </div>
+
+             <div className='asdf'>
+                 <img src={bd2} width='300' height='200' size='30' ></img>
+             </div>
+
+             <div className='rtyu'>
+                 <img src={bd3} width='300' height='200' size='30' ></img>
+             </div>
+
+             <div className='gthy'>
+               <img src={bd5} width='300' height='200' size='30'></img>
+             </div>
+
+             <div className='qsc'>
+                <h3> Scandinavian style apartment</h3>
+             </div>
+
+             <div className='vsl'>
+                 <h4>94-98 Ingraham St,Brooklyn,NY 11237,USA</h4>
+             </div>
+
+             <div className='gk'>
+                <h3> Lovelace Road Greenfield</h3>
+             </div>
+
+             <div className='vky'>
+                 <h4>120 N St SW Thomaston,Georgia(GA),30286</h4>
+             </div>
+
+             <div className='san'>
+                <h3> Building Complex Apartments</h3>
+             </div>
+
+             <div className='sak'>
+                <h4> 319 Brookmade Way Lawrenceville,Georgia(GA),30043</h4>
+             </div>
+
+             <div className='nir'>
+                 <hr></hr>
+             </div>
+
+             <div className='diw'>
+            < AiOutlinePlusCircle size='20'/>
+             </div>
+
+             <div className='kee'>
+                 VIEW MORE
+             </div>
+
+             <div className='kri'>
+               <h2>  Beautifully designed,inside</h2>
+             </div>
+
+             <div className='shn'>
+                <h2> and out, Studio looks every bit</h2>
+             </div>
+
+             <div className='nav'>
+                 <h2>as powerful as it is.</h2>
+             </div>
+
+             <div className='zee'>
+                <h3> This is photoshops's version of Lorem Ipsum. Protin gravida nibh vel velit auctor aliquet.</h3>
+             </div>
+
+             <div className='vijay'>
+                 <h3>Aenean sollicitudin,loreum quis bibendum auctor,nisi elit consequat ipsum,nec sagittis sem</h3>
+             </div>
+
+             <div className='sun'>
+                 <h3>nibh id elit.Duis sed odio sit amet nibh vulputate curses a sit amet mauris.</h3>
+             </div>
+
+             <div className='disn'>
+                 <img src={signature} width='200'></img>
+             </div>
+              <div className='sid1'>
+             <div className='sid'>
+                 <img src={rock} width='500' height='500'></img>
+                 </div>
+             </div>
+
+             <div className='cli'>
+                <h1> Our Clients Review</h1>
+             </div>
+
+             <div className='dio'>
+               <h3>  Duane Neal</h3>
+             </div>
+
+             <div className='pd'> 
+                 <h3>Product Designer, USA</h3>
+             </div>
+
+             <div className='qts'>
+                 <img src={quotes} width='25'></img>
+             </div>
+
+             <div className='free'>
+               <h4>  I'm a freelance fashion designer who specialises in print designs and combining fabrics.My designs have been sold all over Europe and</h4>
+             </div>
+
+             <div className='us'>
+                 <h4>the USA and I have worked with some of the biggest designers in the industry. I'm also currently working with a big supermarket to</h4>
+             </div>
+
+             <div className='devop'>
+                <h4> develop a gorgeous,yet affordable</h4>
+             </div>
+
+             <div className='li'>
+                 <img src={linee}></img>
+             </div>
+
+             <div className='str'>
+                 <img src={str2}  height='90' width='90'></img>
+             </div>
+
+             <div className='st3'>
+                 <img src={str3}  height='90' width='90'></img>
+             </div>
+
+             <div className='st4'>
+                 <img src={str4} height='110' width='110'></img>
+             </div>
+
+             <div className='st5'>
+                 <img src={str5} height='90' width='90'></img>
+             </div>
+
+             <div className='st6'>
+                 <img src={str6} height='90' width='90'></img>
+             </div>
+
+             <div className='do'>
+             <BsDot size='25'/>
+             <BsDot size='25'/>
+             <BsDot size='25'/>
+             <BsDot size='25'/>
+             <BsDot size='25'/>
+             </div>
+
+             <div className='q1'>
+                 <h2> Our Blog</h2>
+             </div>
+
+             <div className='bl1'>
+                 <img src={bul1} width='400' height='400'></img>
+             </div>
+
+             <div className='c1'>
+                <h3> Lorem Ipsum is simply dummy text of</h3>
+             </div>
+             <div className='c2'>
+                 <h3>the printing and typesetting</h3>
+             </div>
+
+             <div className='c3'>
+                <h4>13 March,2020</h4>
+             </div>
+
+             <div className='c4'>
+               <h4>Contrary to popular belief,Lorem Ipsum is not simply random text</h4>
+             </div>
+
+             <div className='c5'>
+                 <h4>It has roots in a piece of classical Latin literature from</h4>
+             </div>
+
+             <div className='c6'>
+                 <img src={bul2} width='250' height='250'></img>
+             </div>
+
+             <div className='c7'>
+                <h4> There are many variations of</h4>
+             </div>
+             <div className='c8'>
+                 <h4>passengers of Lorem Ipsum
+                 </h4>
+
+                 <div className='c9'>
+                 <h4>13 March,2020</h4>
+                 </div>
+
+                 <div className='a1'>
+                     <img src={bul3} width='250' height='150'></img>
+                 </div>
+
+                 <div className='a2'>
+                   <h4>  There are many variations of</h4>
+                 </div>
+
+                 <div className='a3'>
+                   <h4>passages of Lorean Ipsum</h4>
+                 </div>
+
+                 <div className='a4'>
+                 <h4>13 March,2020</h4>
+                 </div>
+
+                 <div className='a5'>
+                 <img src={bul4} width='400' height='250'></img>
+                 </div>
+
+                 <div className='a6'>
+                 <h4>  There are many variations of</h4>
+                 </div>
+
+                 <div className='a7'>
+                 <h4>passages of Lorean Ipsum</h4>
+                 </div>
+
+                 <div className='a8'>
+                 <h4>13 March,2020</h4>
+                 </div>
+
+                 <div className='a9'>
+                 <img src={bul5} width='400' height='150'></img>
+                 </div>
+
+                 <div className='r1'>
+                 <h4>  There are many variations of</h4>
+                 </div>
+
+                 <div className='r2'>
+                 <h4>passages of Lorean Ipsum</h4>
+                 </div>
+
+                 <div className='r3'>
+                 <h4>13 March,2020</h4>
+                 </div>
+
+                 <div className='r4'>
+                     <div className='r14'>
+                   <h1>Every thing is about to change.</h1></div>
+                 </div>
+                 <div className='r15'>
+                   <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit,sed do eiusmod tempor incididunt ut labore et dolore</h3>
+                 <div>
+
+                     <div className='r16'>
+                         <h3>magna aliqua. Ut rnim ad minim veniam,quis nostrud exercitation ullamco laboris.</h3>
+                     </div>
+
+                <div className='r5'>
+                  <input type='text'  placeholder='Email Address'></input>
+                  <div className='r6'>
+                   <button className='btns'>SUBSCRIBE</button>
+                </div>
+                </div>
+
+               
+                     </div>
+
+                 </div>
+                 <div className='s1'>
+                     <img src={streets} width='1500' height='300'></img>
+                 </div>
+                 <div className='s2'><h1>wp</h1></div>
+        <div className='s3'><h1>/</h1></div>
+        <div className='s4'><h2>Real Estate</h2></div>
+        <div className='s5'><h2>HOME</h2></div>
+        <div className='s6'><h2>ABOUT</h2></div>
+        <div className='s7'><h2>AGENTS</h2></div>
+        <div className='s8'><h2>PROPERTIES</h2></div>
+        <div className='s9'><h2>BLOG</h2></div>
+        <div className='s10'><h2>CONTACT</h2></div>
+
+</div>
+<div className='y1'>
+   <h3> ABOUT</h3>
+</div>
+
+<div className='y2'>
+    ABOUT STUDIO
+</div>
+
+<div className='y3'>
+    JOBS
+</div>
+
+<div className='y4'>
+    TEAM
+</div>
+
+<div className='y5'>
+    SECURITY
+</div>
+
+<div className='x1'>
+   <h3> INFORMATION</h3>
+</div>
+
+<div className='x2'>
+    CUSTOMER SERVICE
+</div>
+
+<div className='x3'>
+    LEGAL INFORMATION
+</div>
+
+<div className='x4'>
+    SUPPORT
+</div>
+<div className='x5'>
+    FAQ
+</div>
+
+<div className='v1'>
+   <h3> RECENT NEWS</h3>
+</div>
+
+<div className='v2'>
+    Proin eros nisl,ornare sed<br/>
+    vehicula sit amet,feugiat<br/>
+    <h4>Aug 12|2017</h4>
+
+    <div className='v9'>
+        Morbi placerat accumsan<br/>
+        nunc,luctus lacinia nisi<br/>
+        <h4>Aug 12|2017</h4>
+    </div>
+
+    <div className='v8'>
+   <h3> GET IN TOUCH</h3>
+</div>
+
+<div className='n1'>
+    San Fransisco ,CA<br/>
+    125,california,STP 2 Building
+</div>
+
+<div className='n2'>
+    WWW.studiotheme.com
+</div>
+
+<div className='n3'>
+    + 123 5645 8934
+</div>
+
+<div className='n4'>
+<div className='n5'><BiBasketball size='25'/></div>
+<div className='n6'><RiFacebookCircleLine size='25'/></div>
+<div className='n7'><TiSocialTwitterCircular size='28'/></div>
+<div className='k1'>PRIVACY POLICY </div>
+<div className='k2'> TERMS & CONDITIONS </div>
+</div>
+
+</div>
+
+
+
+
+    </div>
+    )
+}
+
+export default Polk;
